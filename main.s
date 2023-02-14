049eb48ef622b5626b35712f68c9cf6adfa76205
Guzman Funck <jag19@ic.ac.uk>
2023-02-14 11:47:18 +0000

    Simple 8bit memory code

--- main.s
+++ main.s
@@ -9,36 +9,24 @@ main:
 
 	org	0x100		    ; Main code starts here at address 0x100
 start:
-	movlw 	0x0
-	movwf	0x06, A
-	movwf	TRISB, A	    ; Port C all outputs
+	movlw	0x0
+	movwf	TRISD, A	    ; Port D all outputs --> control 
+	movwf	TRISE, A	    ; Port E all outputs --> data
+	movwf	LATD, A
+	incf	LATD, F, A
 	
-	bra 	test
-loop:
-	movff 	0x06, PORTB
-	incf 	0x06, 1, 0
-	movlw   0x03
-	movwf	0x20, A
-	call    delay1
-test:
-	;movwf	0x06, A		    ; Test for end of loop condition
-	movlw 	0x63
-	cpfsgt 	0x06, A
-	bra 	loop		    ; Not yet finished goto start of loop again
-	goto 	0x0		    ; Re-run program from start
+	bra 	rout
 
-;	end	main                ; delay subrutine
-
-delay1:
-	movlw   0x03
-	movwf	0x30, A
-	call delay2
-	decfsz 0x20, 1, 0
-	bra delay1
-	return 
-delay2:
-	decfsz 0x30, 1, 0
-	bra delay2
+delay:
 	return 
 	
-	end
+rout:
+	movlw	0xAE
+	movwf	LATE, A
+	decf    LATD, F, A
+	call	delay
+	incf	LATD, F, A
+	setf	TRISE, A
+	
+	goto $
+	end main
