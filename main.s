74f9dcd8fe9ae3fb85ae89340fcc1bc0afc53bfd
Guzman Funck <jag19@ic.ac.uk>
2023-02-16 11:59:29 +0000

    First cide

--- main.s
+++ main.s
@@ -3,30 +3,30 @@
 psect	code, abs
 	
 	
-main:
-	org	0x0
-	goto	start
+SPI_MasterInit:	    ; Set Clock edge to negative	
+    bcf	    CKE2	; CKE bit in SSP2STAT, 
+    ; MSSP enable; CKP=1; SPI master, clock=Fosc/64 (1MHz)
+    movlw   (SSP2CON1_SSPEN_MASK)|(SSP2CON1_CKP_MASK)|(SSP2CON1_SSPM1_MASK)	
+	; We want to set SSPxCON1 8-bit control register with a value of 
+	; 00110010, but this value without comments is quite difficult to 
+	; interpret. The compiler has 8-bit numbers defined for each of the bit 
+	; flags. For example, for the SSPEN bit in the SSPxCON1 control register 
+	; for SPI mode it has SSP2CON1_SSPEN_MASK defined as 00100000. If we 
+	; 'or' (|) it with the other the other compiler defined masks we can get 
+	; the same number we want to input but in human readable way
+    movwf   SSP2CON1, A	
+    ; SDO2 output; SCK2 output	
+    bcf	    TRISD, PORTD_SDO2_POSN, A	; SDO2 output
+    bcf	    TRISD, PORTD_SCK2_POSN, A	; SCK2 output	
+    return
 
-	org	0x100		    ; Main code starts here at address 0x100
-start:
-	movlw	0x0
-	movwf	TRISD, A	    ; Port D all outputs --> control 
-	movwf	TRISE, A	    ; Port E all outputs --> data
-	movwf	LATD, A
-	incf	LATD, F, A
-	
-	bra 	rout
+SPI_MasterTransmit:  ; Start transmission of data (held in W)
+    movwf   SSP2BUF, A 	; write data to output buffer
 
-delay:
-	return 
-	
-rout:
-	movlw	0xAE
-	movwf	LATE, A
-	decf    LATD, F, A
-	call	delay
-	incf	LATD, F, A
-	setf	TRISE, A
-	
-	goto $
-	end main
+Wait_Transmit:	; Wait for transmission to complete
+    btfss   SSP2IF		; check interrupt flag to see if data has been sent
+    bra	    Wait_Transmit	
+    bcf	    SSP2IF		; clear interrupt flag	
+    return 
+    
+
